2b67e88211850755084c4640b9d5f63635f15d82
NovemberFoxtrot <november@fox.trot>
2013-09-04 22:18:01 -0700

    cool

---
+++ q.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func producer(c chan rune, max int) {
+	defer close(c)
+	for i := 0; i < max; i++ {
+		select {
+		case c <- ' ':
+		case c <- '#':
+		}
+	}
+}
+
+func consumer(c chan rune) {
+	var v rune
+	ok := true
+	for ok {
+		if v, ok = <-c; ok {
+			fmt.Print(string(v))
+		}
+	}
+}
+
+func main() {
+	c := make(chan rune)
+	go producer(c, 10000)
+	consumer(c)
+}
